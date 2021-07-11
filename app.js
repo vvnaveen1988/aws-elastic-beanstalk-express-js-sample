ec20904cfdea9c652e6159519dd1258c2d9a9320
Naveen V V <naveenvv@naveens-mbp.myfiosgateway.com>
2021-07-11 16:14:23 -0400

    The check update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('learning AWS'));
+app.get('/', (req, res) => res.send('The Pipeline build AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
